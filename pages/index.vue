286d65a07cf0f8eb672b346466ec9e1e59008efc
RyutoKawasaki <various.use.0416@gmail.com>
2021-02-02 16:59:20 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="image">
+    <img src="~/assets/nuxtjs-logo.png" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped></style>
